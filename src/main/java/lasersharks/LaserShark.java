5f71cc9cbca67a73ac79285538ae09bee14603d5
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2015-09-03 11:57:15 +0200

    some more javadoc to please checkstyle <documentation

--- src/main/java/lasersharks/LaserShark.java
+++ src/main/java/lasersharks/LaserShark.java
@@ -26,41 +26,75 @@ public class LaserShark implements Fish {
     this.size = size;
   }
 
+  /**
+   * 
+   * @return the direction.
+   */
   public Direction getDirection() {
     return direction;
   }
 
+  /**
+   * 
+   * @param direction the direction to set.
+   */
   public void setDirection(Direction direction) {
     this.direction = direction;
   }
 
+  /**
+   * 
+   * @param position the position to set.
+   */
   public void setPosition(Position position) {
     this.position = position;
   }
 
+  /**
+   * 
+   * @param size the size to set.
+   */
   public void setSize(float size) {
     this.size = size;
   }
 
+  /**
+   * @return the position
+   */
   @Override
   public Position getPosition() {
     return this.position;
   }
 
+  /**
+   * 
+   * @param newDirection the direction to set to.
+   */
   public void setNextMove(Direction newDirection) {
     this.direction = newDirection;
   }
 
+  /**
+   * @return the size.
+   */
   @Override
   public float getSize() {
     return size;
   }
 
+  /**
+   * 
+   * @param fish the fish we want to check if it collides with.
+   * @return true if it collides.
+   */
   public boolean collision(Fish fish) {
     // TODO Auto-generated method stub
     return false;
   }
 
+  /**
+   * moves the shark.
+   */
   @Override
   public boolean move() {
     return getPosition().updatePosition(getDirection());
