c587c8ab372cc124b493bdcefa5283ba039e352f
Daan <daanvanderwerf@hotmail.com>
2015-09-03 13:40:50 +0200

    Removed unused stuff and fixed some checkstyle warnings

--- javaFXGUI/laserSharksGUI/FXGUI.java
+++ javaFXGUI/laserSharksGUI/FXGUI.java
@@ -7,7 +7,6 @@ import javafx.application.Application;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -21,117 +20,108 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class FXGUI extends Application {
-	Label lb_text;
-	private static final int KEYBOARD_MOVEMENT_DELTA = 5;
+  Label lb_text;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-	@Override
-	public void start(Stage stage) throws Exception {
-					
-		StackPane imageContainer = new StackPane();
-        ImageView image = new ImageView("SharkToTheRight.gif");
-        
-        imageContainer.getChildren().addAll(image);
-        
-        Pane pane = new Pane();	
-        pane.getChildren().add(image);
-        
-		final Scene scene = new Scene(pane, 1920, 1080, Color.BLUE);
+  @Override
+  public void start(Stage stage) throws Exception {
 
-		BackgroundImage myBI = new BackgroundImage(new Image(
-				"better background.jpg", 1920, 1080, false, true),
-				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-				BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+    StackPane imageContainer = new StackPane();
+    ImageView image = new ImageView("SharkToTheRight.gif");
 
-		pane.setBackground(new Background(myBI));
-		
+    imageContainer.getChildren().addAll(image);
 
-		moveCircleOnKeyPress(scene, image);
+    Pane pane = new Pane();
+    pane.getChildren().add(image);
 
+    final Scene scene = new Scene(pane, 1920, 1080, Color.BLUE);
 
-        final double rectangleHSpeed = 500 ; // pixels per second
-        final double rectangleVSpeed = 500 ;
-        final double minX = 0 ;
-        final double maxX = 1920 ; 
-        final double minY = 0 ;
-        final double maxY = 1080 ;
+    BackgroundImage myBI = new BackgroundImage(new Image("better background.jpg", 1920, 1080,
+        false, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+        BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
-        final LinkedList<KeyCode> keyStack = new LinkedList<>();
-        scene.setOnKeyPressed(event -> {
-            KeyCode code = event.getCode();
-            if (! keyStack.contains(code)) {
-                keyStack.push(code); 
-            }
-        });
+    pane.setBackground(new Background(myBI));
 
-        scene.setOnKeyReleased(event -> 
-            keyStack.remove(event.getCode()));
+    moveCircleOnKeyPress(scene, image);
 
+    final double rectangleHSpeed = 500; // pixels per second
+    final double rectangleVSpeed = 500;
+    final double minX = 0;
+    final double maxX = 1920;
+    final double minY = 0;
+    final double maxY = 1080;
 
-        final LongProperty lastUpdateTime = new SimpleLongProperty();
-        final AnimationTimer rectangleAnimation = new AnimationTimer() {
-          @Override
-          public void handle(long timestamp) {
-            if (! keyStack.isEmpty() && lastUpdateTime.get() > 0) {
-              final double elapsedSeconds = (timestamp - lastUpdateTime.get()) / 1_000_000_000.0 ;
-              double deltaX = 0 ;
-              double deltaY = 0 ;
-              switch(keyStack.peek()) {
-              case UP:
-                  deltaY = -rectangleVSpeed * elapsedSeconds;
-                  break ;
-              case DOWN: 
-                  deltaY = rectangleVSpeed * elapsedSeconds ;
-                  break ;
-              case LEFT:
-                  deltaX = -rectangleHSpeed * elapsedSeconds ;
-                  break ;
-              case RIGHT:
-                  deltaX = rectangleHSpeed * elapsedSeconds ;
-              default:
-                  break ;
-              }
-              double oldX = image.getTranslateX() ;
-              double oldY = image.getTranslateY() ;
-              image.setTranslateX(clamp(oldX + deltaX, minX, maxX));
-              image.setTranslateY(clamp(oldY + deltaY, minY, maxY));
-            }
-            lastUpdateTime.set(timestamp);
+    final LinkedList<KeyCode> keyStack = new LinkedList<>();
+    scene.setOnKeyPressed(event -> {
+      KeyCode code = event.getCode();
+      if (!keyStack.contains(code)) {
+        keyStack.push(code);
+      }
+    });
+
+    scene.setOnKeyReleased(event -> keyStack.remove(event.getCode()));
+
+    final LongProperty lastUpdateTime = new SimpleLongProperty();
+    final AnimationTimer rectangleAnimation = new AnimationTimer() {
+      @Override
+      public void handle(long timestamp) {
+        if (!keyStack.isEmpty() && lastUpdateTime.get() > 0) {
+          final double elapsedSeconds = (timestamp - lastUpdateTime.get()) / 1_000_000_000.0;
+          double deltaX = 0;
+          double deltaY = 0;
+          switch (keyStack.peek()) {
+            case UP:
+              deltaY = -rectangleVSpeed * elapsedSeconds;
+              break;
+            case DOWN:
+              deltaY = rectangleVSpeed * elapsedSeconds;
+              break;
+            case LEFT:
+              deltaX = -rectangleHSpeed * elapsedSeconds;
+              break;
+            case RIGHT:
+              deltaX = rectangleHSpeed * elapsedSeconds;
+            default:
+              break;
           }
-        };
-        rectangleAnimation.start();
-        
-		stage.setScene(scene);
-		stage.show();
+          double oldX = image.getTranslateX();
+          double oldY = image.getTranslateY();
+          image.setTranslateX(clamp(oldX + deltaX, minX, maxX));
+          image.setTranslateY(clamp(oldY + deltaY, minY, maxY));
+        }
+        lastUpdateTime.set(timestamp);
+      }
+    };
+    rectangleAnimation.start();
 
-	}
-	
-	private double clamp(double value, double min, double max) {
-        return Math.max(min, Math.min(max, value));
-    }
-	
-	private void moveCircleOnKeyPress(Scene scene, final ImageView image) {
-	    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-	      @Override public void handle(KeyEvent event) {
-	        while(event.getCode() == KeyCode.UP){
-	        	//image.setCenterY(image.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
-	        	if (event.KEY_RELEASED != null){
-	        		break;
-	        	}
-	        }
-	      }
-	    });
-	  }
-	
-	
+    stage.setScene(scene);
+    stage.show();
+
+  }
+
+  private double clamp(double value, double min, double max) {
+    return Math.max(min, Math.min(max, value));
+  }
+
+  private void moveCircleOnKeyPress(Scene scene, final ImageView image) {
+    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+      @Override
+      public void handle(KeyEvent event) {
+        while (event.getCode() == KeyCode.UP) {
+          // image.setCenterY(image.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
+          if (KeyEvent.KEY_RELEASED != null) {
+            break;
+          }
+        }
+      }
+    });
+  }
+
 }
