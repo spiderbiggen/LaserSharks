48d551efd7de847f22d1c6383782cfb12cc8bab7
Michiel van den Berg <M.R.vandenBerg-3@student.tudelft.nl>
2015-09-04 23:26:20 +0200

    Added rotating shark and growable shark

--- javaFXGUI/laserSharksGUI/FXGUI.java
+++ javaFXGUI/laserSharksGUI/FXGUI.java
@@ -30,6 +30,9 @@ import javafx.stage.Stage;
 public class FXGUI extends Application {
 	Label lb_text;
 	private static final int KEYBOARD_MOVEMENT_DELTA = 5;
+	private int sizeX = 80;
+	private int sizeY = 40;
+	private double growSize = 1;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -37,15 +40,19 @@ public class FXGUI extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-					
-		StackPane imageContainer = new StackPane();
-        ImageView image = new ImageView("SharkToTheRight.gif");
-        
-        imageContainer.getChildren().addAll(image);
-        
-        Pane pane = new Pane();	
-        pane.getChildren().add(image);
-        
+
+		Image imageright = new Image("SharkToTheRight.gif", sizeX, sizeY, true, false);
+		Image imageleft = new Image("SharkToTheLeft.gif", sizeX, sizeY, true, false);
+		//Image imageright1 = new Image("SharkToTheRight.gif", sizeX * 2, sizeY * 2, true, false);
+		//Image imageleft1 = new Image("SharkToTheLeft.gif", sizeX * 4, sizeY * 4, true, false);
+		ImageView image = new ImageView("SharkToTheRight.gif");
+		
+
+		Pane pane = new Pane();
+		pane.getChildren().add(image);
+		image.setImage(imageright);
+
+
 		final Scene scene = new Scene(pane, 1920, 1080, Color.BLUE);
 
 		BackgroundImage myBI = new BackgroundImage(new Image(
@@ -54,84 +61,74 @@ public class FXGUI extends Application {
 				BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
 		pane.setBackground(new Background(myBI));
-		
 
-		moveCircleOnKeyPress(scene, image);
+		final double rectangleHSpeed = 500; // pixels per second
+		final double rectangleVSpeed = 500;
+		final double minX = 0;
+		final double maxX = 1920;
+		final double minY = 0;
+		final double maxY = 1080;
 
+		final LinkedList<KeyCode> keyStack = new LinkedList<>();
+		scene.setOnKeyPressed(event -> {
+			KeyCode code = event.getCode();
+			if (!keyStack.contains(code)) {
+				keyStack.push(code);
+			}
+		});
 
-        final double rectangleHSpeed = 500 ; // pixels per second
-        final double rectangleVSpeed = 500 ;
-        final double minX = 0 ;
-        final double maxX = 1920 ; 
-        final double minY = 0 ;
-        final double maxY = 1080 ;
+		scene.setOnKeyReleased(event -> keyStack.remove(event.getCode()));
 
-        final LinkedList<KeyCode> keyStack = new LinkedList<>();
-        scene.setOnKeyPressed(event -> {
-            KeyCode code = event.getCode();
-            if (! keyStack.contains(code)) {
-                keyStack.push(code); 
-            }
-        });
+		final LongProperty lastUpdateTime = new SimpleLongProperty();
+		final AnimationTimer rectangleAnimation = new AnimationTimer() {
+			@Override
+			public void handle(long timestamp) {
+				if (!keyStack.isEmpty() && lastUpdateTime.get() > 0) {
+					final double elapsedSeconds = (timestamp - lastUpdateTime
+							.get()) / 1_000_000_000.0;
+					double deltaX = 0;
+					double deltaY = 0;
+					switch (keyStack.peek()) {
+					case UP:
+						deltaY = -rectangleVSpeed * elapsedSeconds;
+						break;
+					case DOWN:
+						deltaY = rectangleVSpeed * elapsedSeconds;
+						break;
+					case LEFT:
+						deltaX = -rectangleHSpeed * elapsedSeconds;
+						image.setImage(imageleft);
+						break;
+					case RIGHT:
+						deltaX = rectangleHSpeed * elapsedSeconds;
+						image.setImage(imageright);
+						break;
+					case H:
+						//image.setImage(imageright1);
+						growSize = growSize * 1.01;
+						image.setScaleX(growSize);
+						image.setScaleY(growSize);
+					default:
+						break;
+					}
+					double oldXr = image.getTranslateX();
+					double oldYr = image.getTranslateY();
+					image.setTranslateX(clamp(oldXr + deltaX, minX, maxX));
+					image.setTranslateY(clamp(oldYr + deltaY, minY, maxY));
 
-        scene.setOnKeyReleased(event -> 
-            keyStack.remove(event.getCode()));
+				}
+				lastUpdateTime.set(timestamp);
+			}
+		};
+		rectangleAnimation.start();
 
-
-        final LongProperty lastUpdateTime = new SimpleLongProperty();
-        final AnimationTimer rectangleAnimation = new AnimationTimer() {
-          @Override
-          public void handle(long timestamp) {
-            if (! keyStack.isEmpty() && lastUpdateTime.get() > 0) {
-              final double elapsedSeconds = (timestamp - lastUpdateTime.get()) / 1_000_000_000.0 ;
-              double deltaX = 0 ;
-              double deltaY = 0 ;
-              switch(keyStack.peek()) {
-              case UP:
-                  deltaY = -rectangleVSpeed * elapsedSeconds;
-                  break ;
-              case DOWN: 
-                  deltaY = rectangleVSpeed * elapsedSeconds ;
-                  break ;
-              case LEFT:
-                  deltaX = -rectangleHSpeed * elapsedSeconds ;
-                  break ;
-              case RIGHT:
-                  deltaX = rectangleHSpeed * elapsedSeconds ;
-              default:
-                  break ;
-              }
-              double oldX = image.getTranslateX() ;
-              double oldY = image.getTranslateY() ;
-              image.setTranslateX(clamp(oldX + deltaX, minX, maxX));
-              image.setTranslateY(clamp(oldY + deltaY, minY, maxY));
-            }
-            lastUpdateTime.set(timestamp);
-          }
-        };
-        rectangleAnimation.start();
-        
 		stage.setScene(scene);
 		stage.show();
 
 	}
-	
+
 	private double clamp(double value, double min, double max) {
-        return Math.max(min, Math.min(max, value));
-    }
-	
-	private void moveCircleOnKeyPress(Scene scene, final ImageView image) {
-	    scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-	      @Override public void handle(KeyEvent event) {
-	        while(event.getCode() == KeyCode.UP){
-	        	//image.setCenterY(image.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
-	        	if (event.KEY_RELEASED != null){
-	        		break;
-	        	}
-	        }
-	      }
-	    });
-	  }
-	
-	
+		return Math.max(min, Math.min(max, value));
+	}
+
 }
