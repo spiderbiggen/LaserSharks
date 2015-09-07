3708abf96c66845e6a94a606eef9bae5b874911c
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2015-09-07 18:05:23 +0200

    better description of the eat function < documentation

--- src/main/java/lasersharks/LaserShark.java
+++ src/main/java/lasersharks/LaserShark.java
@@ -100,7 +100,8 @@ public class LaserShark implements Fish {
   }
   
   /**
-   * The LaserShark eats a fish.
+   * The LaserShark eats a fish. 
+   * As a result: the fish is placed out of the board and the shark grows according to the size of the fish.
    * @param fish the fish the shark eats
    */
   public void eat(Fish fish){
