d31b71b81f7efd3e764f5a24c88fc25e58023eea
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2015-09-10 22:59:26 +0200

    added a getter and setter to the game class <Test

--- src/test/java/lasersharks/GameTest.java
+++ src/test/java/lasersharks/GameTest.java
@@ -1,6 +1,11 @@
 package lasersharks;
 
+import static org.junit.Assert.assertEquals;
+import lasersharks.gui.LevelGUI;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 /**
  * Test class for Game.
  * @author Sytze
@@ -8,11 +13,29 @@ import org.junit.Test;
  */
 public class GameTest {
 
+  Game game;
+  Level level;
+  LevelGUI gui;
+  
+  private final int SCORE = 10;
+  
   /**
-   * We make a new game class.
+   * Set up the game
    */
-  @Test
-  public void testLaunch() {
-    new Game();
+  @Before
+  public void setUp() {
+    game = new Game();
+    level = Mockito.mock(Level.class);
+    gui = Mockito.mock(LevelGUI.class);    
+    game.setLevel(level);
+    Mockito.when(level.getScore()).thenReturn(SCORE);
   }
+  
+  /**
+   * Test the getter
+   */
+  public void testGetLevel() {
+    assertEquals(SCORE, game.getLevel().getScore(), 0);
+  }
+  
 }
