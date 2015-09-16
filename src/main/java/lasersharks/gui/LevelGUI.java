012aadcb60bcda6d734e5d196cd999a20526b4da
Daan <daanvanderwerf@hotmail.com>
2015-09-16 18:47:27 +0200

    Revert "Further transfer of the highscore methods to the new classes"
    This reverts commit 2cdc83c865287bbed765ec9291e70b27d8efa57c.

--- src/main/java/lasersharks/gui/LevelGUI.java
+++ src/main/java/lasersharks/gui/LevelGUI.java
@@ -35,7 +35,6 @@ import javafx.stage.Stage;
 import lasersharks.Direction;
 import lasersharks.Fish;
 import lasersharks.Game;
-import lasersharks.Highscores;
 import lasersharks.LaserShark;
 import lasersharks.Position;
 import lasersharks.ScreenController;
@@ -259,8 +258,8 @@ public class LevelGUI extends Application {
    * @throws IOException
    */
   public void setWinSceneTrue() throws IOException {
-    list = Highscores.readHighscore();
-    Highscores.writeHighscore();
+    list = readHighscore();
+    writeHighscore();
     chooseWinScene = true;
     choosePlayScene = false;
     chooseLoseScene = false;
@@ -272,11 +271,78 @@ public class LevelGUI extends Application {
    * @throws IOException
    */
   public void setLoseSceneTrue() throws IOException {
-    list = Highscores.readHighscore();
-    Highscores.writeHighscore();
+    list = readHighscore();
+    writeHighscore();
     chooseWinScene = false;
     choosePlayScene = false;
     chooseLoseScene = true;
+  }
+
+  /**
+   * Reads the current highscore list so it can be edited.
+   * 
+   * @return the current highscore list.
+   * @throws FileNotFoundException
+   */
+  public static ArrayList<String> readHighscore() throws FileNotFoundException {
+    @SuppressWarnings("resource")
+    Scanner sc = new Scanner(new File("highscores"));
+    ArrayList<String> list = new ArrayList<String>();
+    while (sc.hasNextLine()) {
+      list.add(sc.nextLine());
+    }
+
+    return list;
+
+  }
+
+  /**
+   * Method for writing the highscores. The highscores are saved in highscores.txt .
+   * 
+   * @throws IOException
+   */
+  public static void writeHighscore() throws IOException {
+    for (int i = 0; i < list.size(); i++) {
+      if (score >= Integer.parseInt(list.get(i).substring(3))) {
+        list.remove(i);
+        list.add(i, i + ". " + score);
+        break;
+      }
+
+    }
+    fixHighscoreCount(list);
+
+    try (FileWriter fw = new FileWriter("highscores")) {
+      for (int i = 0; i < list.size(); i++) {
+        if (i < list.size() - 1) {
+          fw.write(list.get(i) + System.lineSeparator());
+        } else {
+          fw.write(list.get(i));
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Fixes the still erroneous list after the new score has been inserted in the writeHighscore()
+   * method. An example would be 1. 500 2. 250 2. 250 3. 100 4. 50.
+   * 
+   * @param list
+   * @return the correct highscore list
+   */
+  public static ArrayList<String> fixHighscoreCount(ArrayList<String> list) {
+    for (int i = 0; i < list.size(); i++) {
+      String newEntry = list.get(i);
+      list.remove(i);
+
+      newEntry = (i + 1) + ". " + newEntry.substring(3);
+      list.add(i, newEntry);
+    }
+
+    return list;
   }
 
   /**
