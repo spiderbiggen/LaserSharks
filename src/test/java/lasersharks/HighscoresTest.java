27944b6190568b6cd31d2a71cfba465b6d0e16cf
Daan <daanvanderwerf@hotmail.com>
2015-09-18 22:20:21 +0200

    Added a teardown method which will be needed for testing the write method < testing

--- src/test/java/lasersharks/HighscoresTest.java
+++ src/test/java/lasersharks/HighscoresTest.java
@@ -21,18 +21,34 @@ public class HighscoresTest {
     Highscores.setInputFile(INPUT_FILE);
   }
 
+  /**
+   * After executing all tests the highscore file should be written back to how it was.
+   * 
+   * @throws Exception
+   */
   @After
   public void tearDown() throws Exception {
+    try (FileWriter fw = new FileWriter(new File(INPUT_FILE))) {
+      for (int i = 0; i < 5; i++) {
+        if (i < 5 - 1) {
+          fw.write((i + 1) + ". " + (i + 1) + System.lineSeparator());
+        } else {
+          fw.write((i + 1) + ". " + (i + 1));
+        }
 
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-  public void rewrite() throws IOException {
-
-  }
-
+  /**
+   * Test case for readHighScores.
+   * 
+   * @throws IOException
+   */
   @Test
   public void testReadHighscores() throws IOException {
-  System.out.println(Highscores.readHighscore()); 
+   
   }
-
 }
