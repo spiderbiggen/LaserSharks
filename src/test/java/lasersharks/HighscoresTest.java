e5474951df3968a990155425c671a5fca30be024
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2015-10-07 18:01:06 +0200

    Removed Syso < bug

--- src/test/java/lasersharks/HighscoresTest.java
+++ src/test/java/lasersharks/HighscoresTest.java
@@ -72,7 +72,6 @@ public class HighscoresTest {
   @Test
   public void testReadHighscoresTrue() throws IOException {
     highscores.readHighscore();
-    System.out.println(highscores.getList());
     assertEquals("[1. 5, 2. 4, 3. 3, 4. 2, 5. 1]", highscores.getList().toString());
   }
 
