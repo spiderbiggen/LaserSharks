e145ce2e09d60b0e3775c36fdbdea563b94b267f
Michiel van den Berg <M.R.vandenBerg-3@student.tudelft.nl>
2015-10-09 23:25:33 +0200

    Added missing javadoc < documentation

--- src/test/java/lasersharks/ScreenControllerTest.java
+++ src/test/java/lasersharks/ScreenControllerTest.java
@@ -1,7 +1,5 @@
 package lasersharks;
 
-import java.io.IOException;
-import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +8,12 @@ import org.mockito.Mockito;
 import javafx.scene.Scene;
 import lasersharksgui.GamePane;
 import lasersharksgui.MainGui;
+
+/**
+ * 
+ * @author SEM Group27
+ *
+ */
 
 @SuppressWarnings("restriction")
 public class ScreenControllerTest {
@@ -23,6 +27,9 @@ public class ScreenControllerTest {
   private static final float ABOVE_WIN_SIZE = 1001.0f;
   private static final float UNDER_WIN_SIZE = 999.0f;
 
+  /**
+   * Set up which is executed before every test.
+   */
   @Before
   public void setUp() {
     currentPane = Mockito.mock(GamePane.class);
@@ -37,17 +44,26 @@ public class ScreenControllerTest {
     screenCon = new ScreenController(currentPane);
   }
 
+  /**
+   * Test for the constructer.
+   */
   @Test
   public void testConstructor() {
     Mockito.verify(currentPane).setScreenController(screenCon);
   }
 
+  /**
+   * Test for the start method.
+   */
   @Test
   public void testStartGame() {
     screenCon.start();
     Mockito.verify(currentPane).startGame();
   }
 
+  /**
+   * Test for the getShark method.
+   */
   @Test
   public void testGetShark() {
     screenCon.getShark();
