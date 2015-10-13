ec33318b891f0c0985a71a41379f3b21bb8f3b29
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2015-10-13 16:04:27 +0200

    added javadoc, correct filenames and the getStrength() method <feature, documentation

--- src/main/java/lasersharks/LaserBullet.java
+++ src/main/java/lasersharks/LaserBullet.java
@@ -1,26 +1,56 @@
 package lasersharks;
 
+/**
+ * This class represents a laser projectile.
+ * @author sytze
+ *
+ */
 public class LaserBullet extends Fish {
 
-  private static final String LASER_IMAGE = "";
+  private static final String LASER_IMAGE = "greenLaserRay.png";
   private static final double LASER_DEFAULT_STRENGTH = 1.0;
+  private static final double IMG_WIDTH = 290;
+  private static final double IMG_HEIGHT = 74;
   
+  private double strength;
+  
+  /**
+   * The constructor of the laser
+   * @param position the position of the laser.
+   * @param size the size of the laser.
+   * @param startSpeed the starting speed of the laser.
+   * @param direction the direction of the laser.
+   */
   public LaserBullet(Position position, float size, double startSpeed, Direction direction) {
     super(position, size, startSpeed, direction);
     collisionBehaviour = new DefaultCollisionBehaviour(this);
     eatBehaviour = new CantEatBehaviour(this);
     moveBehaviour = new BotMoveBehaviour(this);
+    strength = LASER_DEFAULT_STRENGTH;
   }
 
+  /**
+   * gets the filename of the image used for the laser.
+   */
   @Override
   public String getImageResource() {
     return LASER_IMAGE;
   }
 
+  /**
+   * gets the widthscale of the image. (width/height)
+   */
   @Override
   public double getWidthScale() {
-    // TODO Auto-generated method stub
-    return 0;
+    return IMG_WIDTH/IMG_HEIGHT;
+  }
+  
+  /**
+   * returns the strength.
+   * @return the strength.
+   */
+  public double getStrength() {
+    return strength;
   }
   
 }
