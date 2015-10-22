0492baf9655fdaeb973c6affa33695c29a412312
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2015-10-22 23:17:47 +0200

    Removal of aditional logic in the shootController class < optimalization

--- src/main/java/lasersharks/controllers/ShootController.java
+++ src/main/java/lasersharks/controllers/ShootController.java
@@ -10,10 +10,10 @@ import javafx.scene.input.KeyEvent;
  * @author SEMGroup27
  *
  */
+@SuppressWarnings("restriction")
 public class ShootController implements EventHandler<KeyEvent> {
 
   private FishController fishController;
-  private boolean pressedSpace;
 
   /**
    * The constructor of the class.
@@ -24,19 +24,12 @@ public class ShootController implements EventHandler<KeyEvent> {
   public ShootController(FishController fishController) {
     super();
     this.fishController = fishController;
-    pressedSpace = false;
   }
 
   @Override
   public void handle(KeyEvent event) {
-    if (event.getCode() == KeyCode.L) {
-      if (event.getEventType() == KeyEvent.KEY_PRESSED && !pressedSpace) {
+    if (event.getCode() == KeyCode.L && event.getEventType() == KeyEvent.KEY_PRESSED) {
         shoot();
-        pressedSpace = true;
-      }
-      if (event.getEventType() == KeyEvent.KEY_RELEASED && pressedSpace) {
-        pressedSpace = false;
-      }
     }
   }
 
