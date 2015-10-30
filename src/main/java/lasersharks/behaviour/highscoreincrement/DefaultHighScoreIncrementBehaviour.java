226b96be597f263620d40f3b7144361ec1b6d51c
Daan <daanvanderwerf@hotmail.com>
2015-10-30 18:03:11 +0100

    Commented merge conflict removed < documentation

--- src/main/java/lasersharks/behaviour/highscoreincrement/DefaultHighScoreIncrementBehaviour.java
+++ src/main/java/lasersharks/behaviour/highscoreincrement/DefaultHighScoreIncrementBehaviour.java
@@ -3,10 +3,8 @@ package lasersharks.behaviour.highscoreincrement;
 import lasersharks.behaviour.HighScoreIncrementBehaviour;
 
 /**
- * High score Increment behaviour for SeaObjects. <<<<<<< HEAD =======
+ * High score Increment behaviour for SeaObjects.
  * 
- * @author SEMGroup27 >>>>>>> remotes/origin/master
- *
  * @author SEMGroup27
  */
 public class DefaultHighScoreIncrementBehaviour implements HighScoreIncrementBehaviour {
