75a8dd4c48b0209786ad9f8eeb36eea54602950b
Youri Arkesteijn <yarkesteijn@student.tudelft.nl>
2015-10-30 23:15:11 +0100

    Added javadoc to CollisionHitBoxBehaviourComponent < documentation

--- src/main/java/lasersharks/behaviour/collision/component/OnCollisionEatenComponent.java
+++ src/main/java/lasersharks/behaviour/collision/component/OnCollisionEatenComponent.java
@@ -2,15 +2,27 @@ package lasersharks.behaviour.collision.component;
 
 import lasersharks.interfaces.CollisionComponent;
 import lasersharks.interfaces.Displayable;
-
+/**
+ * Class for propagation on collision, this part propagates the eaten event.
+ * @author SEMGroup27
+ *
+ */
 public class OnCollisionEatenComponent implements CollisionComponent {
 
   private final CollisionComponent next;
 
+  /**
+   * Constructor.
+   * @param next component to be called after this one.
+   */
   public OnCollisionEatenComponent(final CollisionComponent next) {
     this.next = next;
   }
 
+  /**
+   * Handle the collision.
+   * @param other The element on with wich the component is interacting.
+   */
   public void handleCollision(final Displayable other) {
     other.onCollisionEaten();
     next.handleCollision(other);
